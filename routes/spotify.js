665e1da12dd8fb035d0f181e465df5f7bad794e4
Simon Colmer <simon_colmer@hotmail.com>
2015-07-01 12:05:36 +0200

    Added cookie encryption

---
+++ routes/spotify.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var config = require('../config/config');
+
+/* GET home page. */
+router.get('/authenticate', function(req, res, next) {
+
+
+
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
