d02d1174387d6f3783071bd7290fd71cf7986cee
Simon Colmer <simon_colmer@hotmail.com>
2015-07-01 12:13:11 +0200

    Fixed spotify routing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var spotify = require('./routes/spotify-login');
+var spotify = require('./routes/spotify');
 
 var app = express();
 
